58743cc9b0949bfed591e1d343af17fc2bdac43b
joydotcom <msjoyjackson@gmail.com>
2021-04-14 13:37:58 -0400

    added variables

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var PORT = process.env.PORT || 3001;
-...
-app.listen(PORT, function() {
+const express = require("express"); 
+const path = require("path");
+const app = express();
 
-}
+
+var PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function() {
+    console.log("server is listening")
+});
 
 app.get("/", function(req, res) {
     res.json(path.join(__dirname, "public/index.html"));
   });
+
+ 
